6596784f162bf7e4e77f7cd53a5178a88d54de2b
trollpy <tshivhenga18@gmail.com>
2025-03-11 14:40:20 +0200

    this is my commit 2

---
+++ python.py
@@ -0,0 +1,24 @@
+# print("hello world!")
+# '''
+# n = 10
+# print(n)
+
+# for i in range (n):
+#     print(i)
+# ''' 
+    
+# #data types
+# a = 10 #int
+# b = 10.5 #float
+# c = "hello" #string
+# d = True #bool
+# e = [1,2,3,4] #list
+# f = (1,2,3,4) #tuple
+# g = {1,2,3,4} #set
+# h = {1: 'one', 2: 'two'} #dict
+
+# print(e[0])
+
+language = ["python", True , 1.3, 1]
+print(language)
+print(language[0], language[1])
